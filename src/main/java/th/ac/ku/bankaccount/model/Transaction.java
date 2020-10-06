5d0b6421bba52e97ed6a41bb1df3d470d8756ddc
Anant Arayanant <minecraftraguna@gmail.com>
2020-10-06 15:16:46 +0700

    use transaction instead of direct edit

---
+++ src/main/java/th/ac/ku/bankaccount/model/Transaction.java
@@ -0,0 +1,57 @@
+package th.ac.ku.bankaccount.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private int bankAccountId;
+    private String transactionType;
+    private double amount;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getBankAccountId() {
+        return bankAccountId;
+    }
+
+    public void setBankAccountId(int bankAccountId) {
+        this.bankAccountId = bankAccountId;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String toString(){
+        return "Transaction{" + "id=" + id + ", bankAccountId=" + bankAccountId + ", " +
+                "transactionType='" + transactionType + '\'' +
+                ", amount=" + amount + '}';}
+}
